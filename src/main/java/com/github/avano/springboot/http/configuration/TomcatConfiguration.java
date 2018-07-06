3f63638144a35539d7956f2a29c56c194ec951b2
Andrej Vano <avano@redhat.com>
2018-07-06 16:11:59 +0200

    [HTTPS] Add HTTPS endpoint

--- src/main/java/com/github/avano/springboot/http/configuration/TomcatConfiguration.java
+++ src/main/java/com/github/avano/springboot/http/configuration/TomcatConfiguration.java
@@ -1,17 +1,24 @@
 package com.github.avano.springboot.http.configuration;
 
+import org.apache.catalina.connector.Connector;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
 
 /**
- * Adds tomcat connector customizer that allows TRACE requests.
+ * Adds:
+ *   - connector customizer that allows TRACE request.
+ *   - HTTP connector.
  */
 @Component
 public class TomcatConfiguration implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 	@Override
 	public void customize(TomcatServletWebServerFactory server) {
 		server.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setAllowTrace(true));
+		final Connector http = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		http.setPort(8080);
+		server.addAdditionalTomcatConnectors(http);
 	}
 }
