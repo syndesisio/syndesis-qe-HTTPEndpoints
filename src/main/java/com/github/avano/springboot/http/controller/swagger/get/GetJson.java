f3244635e51b6ae24032b27fa685ac99ce3a35c8
Andrej Vano <avano@redhat.com>
2019-05-02 13:46:12 +0200

    [Json] Add method returning json array

--- src/main/java/com/github/avano/springboot/http/controller/swagger/get/GetJson.java
+++ src/main/java/com/github/avano/springboot/http/controller/swagger/get/GetJson.java
@@ -18,4 +18,25 @@ public class GetJson {
 	public String getJson() {
 		return ResponseUtil.getJsonResponse("get");
 	}
+
+	@RequestMapping(
+			method = RequestMethod.GET,
+			path = "/api/getJsonArray",
+			produces = "application/json"
+	)
+	@ResponseBody
+	public String getJsonArray() {
+		return "["
+				+ "{\"key1\": \"value1\"},"
+				+ "{\"key2\": \"value2\"},"
+				+ "{\"key3\": \"value3\"},"
+				+ "{\"key4\": \"value4\"},"
+				+ "{\"key5\": \"value5\"},"
+				+ "{\"key6\": \"value6\"},"
+				+ "{\"key7\": \"value7\"},"
+				+ "{\"key8\": \"value8\"},"
+				+ "{\"key9\": \"value9\"},"
+				+ "{\"key10\": \"value10\"}"
+				+ "]";
+	}
 }
